e8323b095132f469dcbb99968befad1085df1a62
Ziiiiing <cheng904@umn.edu>
2020-12-15 20:25:49 +0800

    Dec re-accession
    1. Update the parsing script
    
    2. Upload the Dec re-accession output csv

--- soupXML-to-CSV-Pasda.py
+++ soupXML-to-CSV-Pasda.py
@@ -1,59 +1,78 @@
-# import libraries
+'''
+The script aims to parse HTML elements for PASDA(https://www.pasda.psu.edu) 
+and extract parsed content into a local CSV. The progress is maintained on GitHub
+(https://github.com/BTAA-Geospatial-Data-Project/parse-html).
+
+
+Files
+-----
+pasdaURLs.csv
+	A local csv file stores existing urls that are prepared to parse.
+output_yyyymmdd.csv
+	The output file after parsing and it is followed by the action date.
+
+
+Developers
+----------
+Original created on xxxxx
+Created by Karen Majewicz  @karenmajewicz
+
+Updated December 14, 2020
+Updated by Ziying Cheng  @Ziiiiing
+
+'''
 
 import csv
-import re
-import urllib2
-import csv
-from urllib2 import urlopen
-
-# from urllib.request import urlopen
+import time
+import urllib.request
 from bs4 import BeautifulSoup
 
-contents = []
+# extract exising urls from local csv file
+urls = []
 
-f = csv.writer(open('output_20190905.csv', 'w'))
-f.writerow(['Title','Date','Publisher','Description','Metadata'])
-
-with open('pasdaURLs.csv','r') as csvf: # Open file in read mode
-    urls = csv.reader(csvf)
-    for url in urls:
-        contents.append(url) # Add each url to list contents
-
-for url in contents:  # Parse through each url in the list.
-	page = urlopen(url[0]).read()
-	soup = BeautifulSoup(page, "html.parser")
-
-	titleField = soup.find(attrs={'id': 'Label1'})
-	dateField = soup.find(attrs={'id': 'Label2'})
-	publisherField = soup.find(attrs={'id': 'Label3'})
-	descriptionField = soup.find(attrs={'id': 'Label14'})
-	metadataLink = soup.find('a', href=True, text='Metadata')
-# 	downloadLink = soup.find('a', href=True, text='Download')
-
-	title = titleField.text.strip(),
-	date = dateField.text.strip(),
-	publisher = publisherField.text.strip(),
-	description = descriptionField.text.strip(),
-	metadata = metadataLink['href'],
-# 	download = downloadLink['href']
+with open('pasdaURLs.csv') as fr:
+    reader = csv.reader(fr)  # reader object
+    for row in reader:
+        urls.append(row)
 
 
+# store parsed elements for all urls
+parseElements = []
 
+for url in urls:
+    page = urllib.request.urlopen(url[0]).read()
+    soup = BeautifulSoup(page, "html.parser")
+    print(f'Parsing {url[0]}')
 
-	f.writerow([title,date,publisher,description,metadata])
+    titleField = soup.find(attrs={'id': 'Label1'})
+    dateField = soup.find(attrs={'id': 'Label2'})
+    publisherField = soup.find(attrs={'id': 'Label3'})
+    descriptionField = soup.find(attrs={'id': 'Label14'})
+    metadataLink = soup.find('a', href=True, text='Metadata')
+    # downloadLink = soup.find('a', href=True, text='Download')
 
+    title = titleField.text.strip()
+    #title2 = titleField.get_text()
+    date = dateField.text.strip()
+    publisher = publisherField.text.strip()
+    description = descriptionField.text.strip()
+    metadata = metadataLink['href']
+    # download = downloadLink['href']
+    
+    parseElements.append([title,date,publisher,description,metadata])
+    
+    
+# generate action date with format YYYYMMDD
+actionDate = time.strftime('%Y%m%d')
 
+# write outputs to local csc file
+with open(f'output_{actionDate}.csv', 'w') as fw:
+    fields = ['Title','Date','Publisher','Description','Metadata']
+    
+    writer = csv.writer(fw)   
+    writer.writerow(fields)           # fieldnames
+    writer.writerows(parseElements)   # elements
 
-
-
-
-
-
-
-
-
-
-
-
+    print('#### Job done ####')
 
 
